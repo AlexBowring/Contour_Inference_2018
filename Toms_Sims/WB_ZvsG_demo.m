9002035da2a85f269ed3cbc474d829cd203503c9
nicholst <thomas.nichols@bdi.ox.ac.uk>
2018-11-09 15:59:40 +0000

    Now should match CoPE method

--- Toms_Sims/WB_ZvsG_demo.m
+++ Toms_Sims/WB_ZvsG_demo.m
@@ -18,7 +18,7 @@ set(0, 'DefaultFigureRenderer', 'painters');
 
 
 nSub=60;
-nVox=10000;
+nVox=1000;
 nB=1000;
 nMC=500;
 Alph=[0.8 0.9 0.95];
@@ -31,20 +31,28 @@ Tmax=@(X,df,nV)tcdf(X,df).^nV;
 Y=randn(nSub,nVox,nMC);
 [MTG,MTR]=deal(zeros(nB,nMC));
 
-MTG(1,:)=max(Tstat(Y));
-MTR(1,:)=MTG(1,:);
 
-for i=2:nB
+for i=1:nB
 
+  % Standardised residuals
   res = (Y-mean(Y))./std(Y);
 
+  if i==1
+
+    MTG(1,:)=max(Tstat(Y));
+    MTR(1,:)=MTG(1,:);
+
+  else
+
+    YG=Y.*   randn([nSub 1 nMC]);
+    YR=Y.*(2*randi(2,[nSub 1 nMC])-3);
+
+    MTG(i,:)=max(Tstat(YG));
+    MTR(i,:)=max(Tstat(YR));
+
+  end
+
   fprintf('.')
-
-  YG=Y.*   randn([nSub 1 nMC]);
-  YR=Y.*(2*randi(2,[nSub 1 nMC])-3);
-
-  MTG(i,:)=max(Tstat(YG));
-  MTR(i,:)=max(Tstat(YR));
 
 end
 
