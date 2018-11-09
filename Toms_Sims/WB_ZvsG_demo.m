58bcf4ec936b6b5ea4915a1d88e52bbb924d0f01
nicholst <thomas.nichols@bdi.ox.ac.uk>
2018-11-09 20:42:37 +0000

    Working function (several bugs fixed)

--- Toms_Sims/WB_ZvsG_demo.m
+++ Toms_Sims/WB_ZvsG_demo.m
@@ -12,19 +12,22 @@
 % Bootstrap samples to estimate 0.8, 0.9 and 0.95 quantiles.
 %
 
-clear
 close all
 set(0, 'DefaultFigureRenderer', 'painters');
 
 nSub = 60;
 nVox = 1000;
 nB   = 1000;
-nMC  = 500;
+nMC  = 50;
 Alph = [0.8 0.9 0.95];
-JASA = false;
+%Resid= true;
+%JASA = true;
 % If JASA==true the WB samples are summed and scaled by n^(-1/2) (assuming true variance 1.0)
 % If JASA==false the WB samples are averaged, divided by stdev and scaled by n^(1/2) (reflecting actual studentisation in confidence statement)
 
+if Resid, str1='Resid'; else str1='Raw'; end
+if JASA;  str2='JASA';  else str2='Stud'; end
+fnm=sprintf('S%d_v%d_B%d_MC%d_%s_%s',nSub,nVox,nB,nMC,str1,str2);
 
 if JASA
   Stat = @(X)sum(X)/sqrt(size(X,1));
@@ -41,15 +44,22 @@ Y=randn(nSub,nVox,nMC);
 
 for i=1:nB
 
-  % Standardised residuals
-  res = (Y-mean(Y))./std(Y);
-
   if i==1
 
-    MTG(1,:) = max(Stat(Y));
-    MTR(1,:) = max(Stat(Y));
+    if Resid
+      % If we center the statistic will be exactly zero, so we just scale
+      [MTG(1,:),MTR(1,:)] = deal(max(Stat(Y./std(Y))));
+    else
+      [MTG(1,:),MTR(1,:)] = deal(max(Stat(Y        )));
+    end
+
 
   else
+
+    if i==2 && Resid
+      % Standardised residuals (just once)
+      Y = (Y-mean(Y))./std(Y);
+    end
 
     YG = Y .*    randn(  [nSub 1 nMC]);
     YR = Y .* (2*randi(2,[nSub 1 nMC])-3);
@@ -77,9 +87,9 @@ else
 end
 
 optopt = optimoptions('fmincon','Display','off');
-u80 = fmincon(@(x)(MaxCDF(x,nSub-1,nVox)-0.80).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
-u90 = fmincon(@(x)(MaxCDF(x,nSub-1,nVox)-0.90).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
-u95 = fmincon(@(x)(MaxCDF(x,nSub-1,nVox)-0.95).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
+u80 = fmincon(@(x)(MaxCDF(x,df,nVox)-0.80).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
+u90 = fmincon(@(x)(MaxCDF(x,df,nVox)-0.90).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
+u95 = fmincon(@(x)(MaxCDF(x,df,nVox)-0.95).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
 
 xx=linspace(MnMx(1),MnMx(2),100);
 xxMC=(1:nMC)/(nMC+1);
@@ -93,12 +103,12 @@ plot(prctile(MTG,95),0.95,'.','markersize',10)
 h=[abline('h',[0.8 0.9 0.95]);abline('v',[u80 u90 u95])];
 %set(h,'linestyle','-');
 uistack(h,'top')
-h2=plot(xx,MaxCDF(xx,nSub-1,nVox),'color',[0.8 0 0 ],'linewidth',2);
+h2=plot(xx,MaxCDF(xx,df,nVox),'color',[0.8 0 0 ],'linewidth',2);
 h3=plot(sort(MTG(1,:)),xxMC,'--','color',[0.5 1 0.5],'linewidth',2);
 ylim([0.75 1]);xlim(MnMx)
 legend([h2 h3 h1(1)],'F_{Max} true','F_{Max} MC','F_{Max} Boot','location','southeast')
 hold off
-print -dpdf WB_Gauss.pdf
+print('-dpdf',['WB_Gauss_' fnm '.pdf']);
 
 h1=plot(sort(MTR),xxB);title('Rademacher')
 hold on;
@@ -106,10 +116,10 @@ plot(prctile(MTR,95),0.95,'.','markersize',10)
 h=[abline('h',[0.8 0.9 0.95]);abline('v',[u80 u90 u95])];
 %set(h,'linestyle','-');
 uistack(h,'top')
-h2=plot(xx,MaxCDF(xx,nSub-1,nVox),'color',[0.8 0 0 ],'linewidth',2);
+h2=plot(xx,MaxCDF(xx,df,nVox),'color',[0.8 0 0 ],'linewidth',2);
 h3=plot(sort(MTG(1,:)),xxMC,'--','color',[0.5 1 0.5],'linewidth',2);
 ylim([0.75 1]);xlim(MnMx)
 legend([h2 h3 h1(1)],'F_{Max} true','F_{Max} MC','F_{Max} Boot','location','southeast')
 hold off
 
-print -dpdf WB_Rade.pdf
+print('-dpdf',['WB_Rade_' fnm '.pdf']);
