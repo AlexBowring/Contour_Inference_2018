98e5c0f8d82a229a56077e22173a636f4d4a7b0a
nicholst <thomas.nichols@bdi.ox.ac.uk>
2018-11-09 17:21:33 +0000

    Now can run original JASA & studentised method

--- Toms_Sims/WB_ZvsG_demo.m
+++ Toms_Sims/WB_ZvsG_demo.m
@@ -16,21 +16,28 @@ clear
 close all
 set(0, 'DefaultFigureRenderer', 'painters');
 
+nSub = 60;
+nVox = 1000;
+nB   = 1000;
+nMC  = 500;
+Alph = [0.8 0.9 0.95];
+JASA = false;
+% If JASA==true the WB samples are summed and scaled by n^(-1/2) (assuming true variance 1.0)
+% If JASA==false the WB samples are averaged, divided by stdev and scaled by n^(1/2) (reflecting actual studentisation in confidence statement)
 
-nSub=60;
-nVox=1000;
-nB=1000;
-nMC=500;
-Alph=[0.8 0.9 0.95];
 
-Tstat = @(X)mean(X)./std(X)*sqrt(size(X,1));
-Xbar  = @(X)mean(X)*sqrt(size(X,1));
+if JASA
+  Stat = @(X)sum(X)/sqrt(size(X,1));
+else
+  Stat = @(X)mean(X)./std(X)*sqrt(size(X,1));
+end
 
-Tmax=@(X,df,nV)tcdf(X,df).^nV;
+MaxCDF=@(X,df,nV)tcdf(X,df).^nV;
 
+% Monster data matrix
+% Subjects x Voxels x Monte Carlo realisations
 Y=randn(nSub,nVox,nMC);
 [MTG,MTR]=deal(zeros(nB,nMC));
-
 
 for i=1:nB
 
@@ -39,16 +46,16 @@ for i=1:nB
 
   if i==1
 
-    MTG(1,:)=max(Tstat(Y));
-    MTR(1,:)=MTG(1,:);
+    MTG(1,:) = max(Stat(Y));
+    MTR(1,:) = max(Stat(Y));
 
   else
 
-    YG=Y.*   randn([nSub 1 nMC]);
-    YR=Y.*(2*randi(2,[nSub 1 nMC])-3);
+    YG = Y .*    randn(  [nSub 1 nMC]);
+    YR = Y .* (2*randi(2,[nSub 1 nMC])-3);
 
-    MTG(i,:)=max(Tstat(YG));
-    MTR(i,:)=max(Tstat(YR));
+    MTG(i,:) = max(Stat(YG));
+    MTR(i,:) = max(Stat(YR));
 
   end
 
@@ -56,44 +63,51 @@ for i=1:nB
 
 end
 
-% Find true critical values
+%
+% Plotting
+%
 
 MnMx=[min(MTG(:)), max(MTG(:))];
 
-optopt = optimoptions('fmincon','Display','off');
-u80 = fmincon(@(x)(Tmax(x,nSub-1,nVox)-0.80).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
-u90 = fmincon(@(x)(Tmax(x,nSub-1,nVox)-0.90).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
-u95 = fmincon(@(x)(Tmax(x,nSub-1,nVox)-0.95).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
+% Find true critical values
+if JASA
+  df = Inf;
+else
+  df = nSub-1;
+end
 
+optopt = optimoptions('fmincon','Display','off');
+u80 = fmincon(@(x)(MaxCDF(x,nSub-1,nVox)-0.80).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
+u90 = fmincon(@(x)(MaxCDF(x,nSub-1,nVox)-0.90).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
+u95 = fmincon(@(x)(MaxCDF(x,nSub-1,nVox)-0.95).^2,mean(MnMx),[],[],[],[],MnMx(1),MnMx(2),[],optopt);
 
 xx=linspace(MnMx(1),MnMx(2),100);
-xMC=(1:nMC)/(nMC+1);
-xB=(1:nB)/(nB+1);
-
+xxMC=(1:nMC)/(nMC+1);
+xxB=(1:nB)/(nB+1);
 
 set(gcf,'PaperSize',[4 3]*2.54,'PaperPosition',[0 0 [4 3]*2.54])
 
-h1=plot(sort(MTG),xB);title('Gaussian')
+h1=plot(sort(MTG),xxB);title('Gaussian')
 hold on;
 plot(prctile(MTG,95),0.95,'.','markersize',10)
 h=[abline('h',[0.8 0.9 0.95]);abline('v',[u80 u90 u95])];
 %set(h,'linestyle','-');
 uistack(h,'top')
-h2=plot(xx,Tmax(xx,nSub-1,nVox),'color',[0.8 0 0 ],'linewidth',4);
-h3=plot(sort(MTG(1,:)),xMC,'--','color',[0.5 1 0.5],'linewidth',3);
+h2=plot(xx,MaxCDF(xx,nSub-1,nVox),'color',[0.8 0 0 ],'linewidth',2);
+h3=plot(sort(MTG(1,:)),xxMC,'--','color',[0.5 1 0.5],'linewidth',2);
 ylim([0.75 1]);xlim(MnMx)
 legend([h2 h3 h1(1)],'F_{Max} true','F_{Max} MC','F_{Max} Boot','location','southeast')
 hold off
 print -dpdf WB_Gauss.pdf
 
-h1=plot(sort(MTR),xB);title('Rademacher')
+h1=plot(sort(MTR),xxB);title('Rademacher')
 hold on;
 plot(prctile(MTR,95),0.95,'.','markersize',10)
 h=[abline('h',[0.8 0.9 0.95]);abline('v',[u80 u90 u95])];
 %set(h,'linestyle','-');
 uistack(h,'top')
-h2=plot(xx,Tmax(xx,nSub-1,nVox),'color',[0.8 0 0 ],'linewidth',4);
-h3=plot(sort(MTG(1,:)),xMC,'--','color',[0.5 1 0.5],'linewidth',3);
+h2=plot(xx,MaxCDF(xx,nSub-1,nVox),'color',[0.8 0 0 ],'linewidth',2);
+h3=plot(sort(MTG(1,:)),xxMC,'--','color',[0.5 1 0.5],'linewidth',2);
 ylim([0.75 1]);xlim(MnMx)
 legend([h2 h3 h1(1)],'F_{Max} true','F_{Max} MC','F_{Max} Boot','location','southeast')
 hold off
